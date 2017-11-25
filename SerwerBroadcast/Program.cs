65e89fa7c02cd102271b215f076513c397760f53
Piotrek <piotrek120017@gmail.com>
2017-11-26 00:07:47 +0100

    initial commit

---
+++ SerwerBroadcast/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SerwerBroadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            Byte[] sendByte = Encoding.UTF8.GetBytes("Message");
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 2222);
+            udpClient.Send(sendByte, sendByte.Length, endPoint);
+            udpClient.Close();
+        }
+    }
+}
